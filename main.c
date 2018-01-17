c0a464944b2c6d7cd4d170c31f2718c7bc11081c
Seth Kingsley <sethk@meowfishies.com>
2018-01-16 20:06:32 -0800

    Try to avoid r0 being set

--- main.c
+++ main.c
@@ -1317,6 +1317,13 @@ cpu_exec(const union cpu_inst inst)
 			debug_intr();
 			return false;
 	}
+	//assert(!cpu_state.cs_r[0]);
+	if (cpu_state.cs_r[0])
+	{
+		fputs("r0 written to with non-zero value\n", stderr);
+		cpu_state.cs_r[0] = 0;
+		//debug_intr();
+	}
 	return true;
 }
 
