2dfa35ab7ff5dd2210c1fdb32ade1422b9b1c506
Seth Kingsley <sethk@meowfishies.com>
2018-04-18 14:15:35 -0700

    Mark unused parameters

--- tk_null.c
+++ tk_null.c
@@ -32,12 +32,12 @@ tk_step(void)
 }
 
 void
-tk_blit(const u_int8_t *fb, bool right)
+tk_blit(const u_int8_t *fb __unused, bool right __unused)
 {
 }
 
 void
-tk_debug_draw(u_int x, u_int y, u_int32_t argb)
+tk_debug_draw(u_int x __unused, u_int y __unused, u_int32_t argb __unused)
 {
 }
 
@@ -61,7 +61,7 @@ void tk_fini(void)
 }
 
 enum tk_error_state
-tk_runtime_error(const char *msg, bool allow_always_ignore)
+tk_runtime_error(const char *msg, bool allow_always_ignore __unused)
 {
 	fprintf(stderr, "\n*** %s\n", msg);
 	return ERROR_ABORT;
