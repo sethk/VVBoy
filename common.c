9cdc6215955a1bc3f27f1376429b804e95eee9fa
Seth Kingsley <sethk@meowfishies.com>
2018-08-11 21:06:50 -0700

    Start decoding and displaying sound data

--- common.c
+++ common.c
@@ -318,7 +318,10 @@ mem_write(u_int32_t addr, const void *src, size_t size, u_int *mem_waitp)
 		             debug_format_addr(addr, addr_s), debug_format_hex(src, size, hex_s));
 	}
 
-	switch (size)
+	enum mem_segment seg = MEM_ADDR2SEG(addr);
+	if (seg == MEM_SEG_VSU)
+		vsu_mem_write(&request, src);
+	else switch (size)
 	{
 		case 1:
 			*(u_int8_t *)request.mr_host =
@@ -3314,23 +3317,86 @@ vip_frame_end(void)
 /* VSU */
 struct vsu_ram
 {
-	u_int8_t vr_ram[0x280];
-	u_int32_t vr_sound5[32];
+	u_int8_t vr_waves[5][32];
+	u_int8_t vr_snd5mod[32];
+	u_int8_t vr_rfu[64];
 };
 
 struct vsu_regs
 {
-	u_int8_t vr_regs[0x180];
+	struct vsu_sound_regs
+	{
+		struct
+		{
+			unsigned vi_data : 5 __attribute__((packed));
+			unsigned vi_mode : 1 __attribute__((packed));
+			unsigned vi_rfu1 : 1 __attribute__((packed));
+			unsigned vi_start : 1 __attribute__((packed));
+		} vsr_int;
+		struct
+		{
+			unsigned vl_rlevel : 4 __attribute__((packed));
+			unsigned vl_llevel : 4 __attribute__((packed));
+		} vsr_lrv;
+		u_int8_t vsr_fql;
+		struct
+		{
+			unsigned vf_fqh : 3 __attribute__((packed));
+			unsigned vf_rfu1 : 5 __attribute__((packed));
+		} vsr_fqh;
+		struct
+		{
+			unsigned ve_step : 3 __attribute__((packed));
+			unsigned ve_ud : 1 __attribute__((packed));
+			unsigned ve_init : 4 __attribute__((packed));
+		} vsr_ev0;
+		struct
+		{
+			unsigned ve_on : 1 __attribute__((packed));
+			unsigned ve_rs : 1 __attribute__((packed));
+			unsigned ve_rfu1 : 2 __attribute__((packed));
+			unsigned ve_modswp : 1 __attribute__((packed));
+			unsigned ve_short : 1 __attribute__((packed));
+			unsigned ve_ed : 1 __attribute__((packed));
+			unsigned ve_rfu2 : 1 __attribute__((packed));
+		} vsr_ev1;
+		struct
+		{
+			unsigned vr_addr : 3 __attribute__((packed));
+			unsigned vr_rfu1 : 5 __attribute__((packed));
+		} vsr_ram;
+		struct
+		{
+			unsigned vs_shifts : 3 __attribute__((packed));
+			unsigned vs_ud : 1 __attribute__((packed));
+			unsigned vs_time : 3 __attribute__((packed));
+			unsigned vs_clk : 1 __attribute__((packed));
+		} vsr_swp;
+		u_int32_t vsr_rfu[2];
+	} vr_sounds[6];
 	struct
 	{
 		unsigned vs_stop : 1 __attribute__((packed));
 		unsigned vs_unused : 7 __attribute__((packed));
-	} vr_sstop;
-	u_int8_t vr_regs2[0x7f];
+	} vr_stop;
+	u_int32_t vs_rfu[7];
 };
 
+bool vsu_sounds_open = false;
+bool vsu_buffers_open = false;
 static struct vsu_ram vsu_ram;
 static struct vsu_regs vsu_regs;
+static int16_t vsu_buffers[2][834];
+static u_int8_t vsu_sound_mask = 0b1;
+static struct vsu_state
+{
+	bool vs_started;
+	u_int vs_freq_count;
+	u_int vs_wave_index;
+	u_int vs_int_count;
+	u_int16_t vs_env_value;
+	u_int vs_env_count;
+} vsu_states[6];
 
 bool
 vsu_init(void)
@@ -3357,42 +3423,233 @@ void
 vsu_test(void)
 {
 	debug_printf("Running VSU self-test\n", stderr);
-	mem_test_size("vsu_ram", sizeof(vsu_ram), 0x300);
-	mem_test_size("vsu_regs", sizeof(vsu_regs), 0x200);
-#ifndef NDEBUG
-	assert(mem_emu2host(0x01000580, 1) == &(vsu_regs.vr_sstop));
-#endif // !NDEBUG
+	mem_test_size("vsu_ram", sizeof(vsu_ram), 0x100);
+	mem_test_addr("S1INT", 0x01000400, 1, &(vsu_regs.vr_sounds[0].vsr_int));
+	mem_test_addr("S2INT", 0x01000440, 1, &(vsu_regs.vr_sounds[1].vsr_int));
+	mem_test_addr("S4FQH", 0x010004cc, 1, &(vsu_regs.vr_sounds[3].vsr_fqh));
+	mem_test_addr("SSTOP", 0x01000580, 1, &(vsu_regs.vr_stop));
+	mem_test_size("vsu_regs", sizeof(vsu_regs), 0x80);
 }
 
 void
 vsu_reset(void)
 {
+	for (u_int sound = 0; sound < 6; ++sound)
+		vsu_states[sound].vs_started = false;
 	// TODO
 }
 
 void
 vsu_step(void)
 {
-	// TODO
+	if ((main_usec % 20000) == 0)
+	{
+		for (u_int sample_index = 0; sample_index < 834; ++sample_index)
+		{
+			for (u_int sound = 0; sound < 1; ++sound)
+			{
+				if (!(vsu_sound_mask & (1 << sound)))
+					continue;
+
+				struct vsu_state *state = &(vsu_states[sound]);
+				if (state->vs_started)
+				{
+					const struct vsu_sound_regs *vsr = &(vsu_regs.vr_sounds[sound]);
+					const u_int8_t *vsu_wave = vsu_ram.vr_waves[vsr->vsr_ram.vr_addr];
+					int16_t sample = (vsu_wave[state->vs_wave_index] & 0b111111) - 32;
+					sample*= state->vs_env_value;
+					int16_t left_sample = sample * vsr->vsr_lrv.vl_llevel;
+					int16_t right_sample = sample * vsr->vsr_lrv.vl_rlevel;
+					assert(sample >= -1024 && sample <= 1023);
+					assert((int32_t)sample * 0x40 <= INT16_MAX);
+					assert((int32_t)sample * 0x40 >= INT16_MIN);
+					vsu_buffers[0][sample_index] = left_sample * 0x20;
+					vsu_buffers[1][sample_index] = right_sample * 0x20;
+
+					state->vs_wave_index = (state->vs_wave_index + 1) % 32;
+					if (state->vs_env_count == vsr->vsr_ev0.ve_step)
+					{
+						if (vsr->vsr_ev0.ve_ud)
+						{
+							if (state->vs_env_value == 0xf)
+							{
+								if (vsr->vsr_ev1.ve_rs)
+									state->vs_env_value = 0;
+							}
+							else
+								++state->vs_env_value;
+						}
+						else
+						{
+							if (state->vs_env_value == 0)
+							{
+								if (vsr->vsr_ev1.ve_rs)
+									state->vs_env_value = 0xf;
+							}
+							else
+								--state->vs_env_value;
+						}
+						state->vs_env_count = 0;
+					}
+					else
+						++state->vs_env_count;
+				}
+			}
+		}
+	}
+}
+
+void
+vsu_frame_end(void)
+{
+	if (vsu_sounds_open)
+	{
+		igSetNextWindowSize((struct ImVec2){600, 0}, ImGuiCond_Once);
+		if (igBegin("Sounds", &vsu_sounds_open, 0))
+		{
+			for (u_int sound = 0; sound < 6; ++sound)
+			{
+				char id[32];
+				struct vsu_sound_regs *vsr = &(vsu_regs.vr_sounds[sound]);
+				snprintf(id, sizeof(id), "SOUND%u", sound);
+				char interval_s[16];
+				if (vsr->vsr_int.vi_mode)
+				{
+					u_int int_index = vsr->vsr_int.vi_data;
+					float interval = 1000.0 / (260.4 * (int_index + 1));
+					snprintf(interval_s, sizeof(interval_s), "Int.: %.2f ms (%u)", interval, int_index);
+				}
+				else
+					snprintf(interval_s, sizeof(interval_s), "Continuous");
+
+				u_int16_t freq_index = vsr->vsr_fql;
+				freq_index|= vsr->vsr_fqh.vf_fqh << 8;
+				float freq = 5000000.0 / ((2048 - freq_index) * 32);
+
+				if (igTreeNodeExStr(id, ImGuiTreeNodeFlags_DefaultOpen, "%s%s %s, %.2f Hz (%u), R/L level %u/%u",
+				                    id,
+				                    (vsr->vsr_int.vi_start) ? " (STARTED)" : "",
+				                    interval_s,
+				                    freq, freq_index,
+				                    vsr->vsr_lrv.vl_rlevel,
+									vsr->vsr_lrv.vl_llevel))
+				{
+					float envs[120];
+					u_int8_t env = vsr->vsr_ev0.ve_init;
+					u_int step_time = vsr->vsr_ev0.ve_step + 1;
+					for (u_int i = 0; i < 120; ++i)
+					{
+						envs[i] = env;
+						if (!step_time)
+						{
+							if (vsr->vsr_ev0.ve_ud)
+							{
+								if (env == 0xf)
+								{
+									if (vsr->vsr_ev1.ve_rs)
+										env = 0;
+								}
+								else
+									++env;
+							}
+							else
+							{
+								if (env == 0)
+								{
+									if (vsr->vsr_ev1.ve_rs)
+										env = 0xf;
+								}
+								else
+									--env;
+							}
+							step_time = vsr->vsr_ev0.ve_step;
+						}
+						else
+							--step_time;
+					}
+					char overlay[32];
+					snprintf(overlay, sizeof(overlay), "Init %u, Step %u %s, %s",
+					         vsr->vsr_ev0.ve_init,
+					         vsr->vsr_ev0.ve_step + 1,
+					         (vsr->vsr_ev0.ve_ud) ? "Up" : "Dn",
+					         (vsr->vsr_ev1.ve_rs) ? "R/S" : "No R/S");
+					igPlotLines("Env.", envs, 120, 0, overlay, 0, 0xf, (struct ImVec2){240, 64}, sizeof(float));
+					igSameLine(0, -1);
+					igText("(%s)", (vsr->vsr_ev1.ve_on) ? "ON" : "OFF");
+
+					u_int wave_addr = vsr->vsr_ram.vr_addr % 5;
+					float wave[32];
+					for (u_int i = 0; i < 32; ++i)
+					{
+						int8_t sample = (vsu_ram.vr_waves[wave_addr][i] & 0b111111) - 32;
+						wave[i] = sample;
+					}
+					igSameLine(0, -1);
+					snprintf(overlay, sizeof(overlay), "Waveform RAM %u", wave_addr);
+					igPlotLines("Wave.",
+					            wave,
+					            32, 0,
+					            overlay,
+					            -32, 31,
+					            (struct ImVec2){256, 64},
+					            sizeof(float));
+
+					igTreePop();
+				}
+			}
+		}
+		igEnd();
+	}
+
+	if (vsu_buffers_open)
+	{
+		if (igBegin("Audio Buffers", &vsu_buffers_open, 0))
+		{
+			for (u_int channel = 0; channel < 2; ++channel)
+			{
+				float samples[834];
+				for (u_int i = 0; i < 834; ++i)
+					samples[i] = (double)vsu_buffers[channel][i];
+				igPlotLines((channel == 0) ? "Left" : "Right",
+				            samples, 834, 0,
+				            NULL,
+				            INT16_MIN, INT16_MAX,
+				            (struct ImVec2){834, 128},
+				            sizeof(float));
+			}
+		}
+		igEnd();
+	}
 }
 
 bool
 vsu_mem_prepare(struct mem_request *request)
 {
-	// TODO: More granularity on perms
-
-	if (request->mr_emu + request->mr_size <= 0x01000300)
-		request->mr_host = (u_int8_t *)&vsu_ram + (request->mr_emu & 0x3ff);
-	else if (request->mr_emu + request->mr_size <= 0x01000400)
+	if (request->mr_size != 1)
 	{
-		u_int32_t mirror = request->mr_emu % 0x300;
-		static bool always_ignore = false;
-		if (!debug_runtime_errorf(&always_ignore, "Mirroring VSU RAM at 0x%08x -> 0x%x", request->mr_emu, mirror))
+		static bool ignore_size = false;
+		if (!debug_runtime_errorf(&ignore_size, "Invalid VSU access size %ld @ 0x%08x\n",
+		                          request->mr_size, request->mr_emu))
 			return false;
-		request->mr_host = (u_int8_t *)&vsu_ram + mirror;
+		request->mr_size = 1;
 	}
-	else if (request->mr_emu >= 0x01000400 && request->mr_emu + request->mr_size <= 0x01000600)
-		request->mr_host = (u_int8_t *)&vsu_regs + (request->mr_emu - 0x01000400);
+
+	// TODO: More granularity on perms
+	if (request->mr_emu < 0x01000400)
+	{
+		if (request->mr_emu >= 0x01000300)
+		{
+			u_int32_t mirror = request->mr_emu & 0x010003ff;
+			static bool always_ignore = false;
+			if (!debug_runtime_errorf(&always_ignore, "Mirroring VSU RAM at 0x%08x -> 0x%x", request->mr_emu, mirror))
+				return false;
+			request->mr_emu = mirror;
+		}
+
+		request->mr_host = (u_int8_t *) &vsu_ram + ((request->mr_emu >> 2) & 0xff);
+	}
+	else if (request->mr_emu < 0x01000600)
+		request->mr_host = (u_int8_t *)&vsu_regs + ((request->mr_emu >> 2) & 0x7f);
 	else
 	{
 		if (!debug_runtime_errorf(NULL, "VSU bus error at 0x%08x\n", request->mr_emu))
@@ -3403,6 +3660,52 @@ vsu_mem_prepare(struct mem_request *request)
 	}
 
 	return true;
+}
+
+static void
+vsu_stop_sound(u_int sound)
+{
+	if (vsu_states[sound].vs_started)
+		debug_tracef("vsu", "Stopping SOUND%u\n", sound);
+}
+
+void
+vsu_mem_write(const struct mem_request *request, const void *src)
+{
+	u_int8_t value = *(u_int8_t *)src;
+	*(u_int8_t *)request->mr_host = value;
+	if ((request->mr_emu & 0b10000111111) == 0b10000000000)
+	{
+		u_int sound = (request->mr_emu >> 6) & 0b111;
+		if (sound == 6)
+		{
+			// SSTOP
+			if (value & 1)
+			{
+				for (u_int i = 0; i < 6; ++i)
+					vsu_stop_sound(i);
+			}
+		}
+		else
+		{
+			assert(sound < 6);
+			// SxINT
+			if (value & 0x80)
+			{
+				debug_tracef("vsu", "Starting SOUND%u\n", sound);
+				const struct vsu_sound_regs *vsr = &(vsu_regs.vr_sounds[sound]);
+				struct vsu_state *state = &(vsu_states[sound]);
+				state->vs_started = true;
+				state->vs_freq_count = 0;
+				state->vs_wave_index = 0;
+				state->vs_int_count = 0;
+				state->vs_env_value = vsr->vsr_ev0.ve_init;
+				state->vs_env_count = 0;
+			}
+			else
+				vsu_stop_sound(sound);
+		}
+	}
 }
 
 void
@@ -6258,6 +6561,11 @@ imgui_frame_begin(void)
 			igMenuItemPtr("Objects...", "F4", &vip_oam_open, true);
 			igMenuItemPtr("Frame buffers...", "F5", &vip_fb_open, true);
 
+			igSeparator();
+
+			igMenuItemPtr("Sounds...", NULL, &vsu_sounds_open, true);
+			igMenuItemPtr("Audio buffers...", NULL, &vsu_buffers_open, true);
+
 			igEndMenu();
 		}
 
@@ -6570,6 +6878,7 @@ main_frame(void)
 		debug_tracef("main", "End frame\n");
 
 	vip_frame_end();
+	vsu_frame_end();
 
 	main_draw();
 
